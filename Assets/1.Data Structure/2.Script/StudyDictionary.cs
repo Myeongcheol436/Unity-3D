379d9b7e19611c83245778af3e1ef941f8ee948c
Myeongcheol436 <zkdl436@naver.com>
2025-07-08 14:45:26 +0900

    Dictionary
    study

--- Assets/1.Data Structure/2.Script/StudyDictionary.cs
+++ Assets/1.Data Structure/2.Script/StudyDictionary.cs
@@ -1,30 +1,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StudyDictionary : MonoBehaviour
+
+public class PersonData
 {
-    public Dictionary<string, int> persons = new Dictionary<string, int>();
+    public int age;
+    public string name;
+    public float height;
+    public float weight;
 
-    private void Start()
+    public PersonData(int age, string name, float height, float weight)
     {
-        persons.Add("철수", 10); //key값은 중복 허용x value 값은 중복 허용o
-        persons.Add("영희", 15); //key, value
-        persons.Add("영희", 17); //영희가 덮어 써짐
-        persons.Add("동수", 17);
-
-        int age = persons["철수"];
-        Debug.Log($"철수의 나이는{age}입니다.");
-
-        foreach (var person in persons)
-        {
-            if (person.Value == 15)
-            {
-                Debug.Log($"나이가 15인 사람의 이름은 {person.Key}입니다.");
-            }
-
-            Debug.Log($"{person.Key}의 나이는 {person.Value}입니다.");
-
-        }
-
+        this.age = age;
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
     }
 }
+
+    public class StudyDictionary : MonoBehaviour
+    {
+        public Dictionary<string, PersonData> persons = new Dictionary<string, PersonData>();
+
+        private void Start()
+        {
+            persons.Add("철수", new PersonData(10, "철수", 150f, 30f));
+            persons.Add("영희", new PersonData(10, "영희", 150f, 30f));
+            persons.Add("동수", new PersonData(10, "동수", 150f, 30f));
+
+            Debug.Log(persons["철수"].age);
+            Debug.Log(persons["철수"].name);
+            Debug.Log(persons["철수"].height);
+            Debug.Log(persons["철수"].weight);
+        }
+    }
+
